26f9e133daafd69c2b044596d6872d4a6eda6424
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2020-06-29 11:44:22 +0100

    use fish_user_paths path to avoid duplication

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 #theme_gruvbox dark hard
 set fish_greeting ""
-set -gx PATH $PATH $HOME/.nimble/bin
-set -gx PATH $PATH $HOME/.cargo/bin
-set -gx PATH $PATH $HOME/go/bin
+set -gx fish_user_paths $fish_user_paths  $HOME/.nimble/bin
+set -gx fish_user_paths $fish_user_paths $HOME/.cargo/bin
+set -gx fish_user_paths $fish_user_paths $HOME/go/bin
 set -Ux EDITOR nvim
 alias vim=nvim
