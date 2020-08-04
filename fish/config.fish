81757a4fc9c83ac8f1ca0f536e577bc70ce3d1c9
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2020-08-04 09:48:14 +0100

    added doom bin to fish path

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,6 @@ set -gx fish_user_paths $fish_user_paths  $HOME/.nimble/bin
 set -gx fish_user_paths $fish_user_paths $HOME/.cargo/bin
 set -gx fish_user_paths $fish_user_paths $HOME/go/bin
 set -gx fish_user_paths $fish_user_paths $HOME/.vim/plugged/vim-iced/bin
+set -gx fish_user_paths $fish_user_paths $HOME/.emacs.d/bin
 set -Ux EDITOR nvim
 alias vim=nvim
