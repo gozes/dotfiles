fc59c2f081c6d21c185c946a0083dc2d73b1d370
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2021-10-29 16:34:28 +0100

    More adjustment to telescope keymaps

--- nvim/lua/kit/keymaps.lua
+++ nvim/lua/kit/keymaps.lua
@@ -14,8 +14,8 @@ nnoremap <C-g> <cmd>Telescope live_grep<Cr>
 nnoremap <leader><C-t>f <cmd>Telescope file_browser<Cr>
 nnoremap <leader><C-t>h <cmd>Telescope help_tags<Cr>
 nnoremap <leader><C-t>q <cmd>Telescope quickfix<Cr>
-nnoremap <leader><C-t>s <cmd>Telescope spell_suggest<Cr>
-nnoremap <leader><C-t>ds <cmd>Telescope lsp_document_symbols<Cr>
+nnoremap <leader><C-t>S <cmd>Telescope spell_suggest<Cr>
+nnoremap <leader><C-t>s <cmd>Telescope lsp_document_symbols<Cr>
 nnoremap <leader><C-t>o <cmd>Telescope vim_options<Cr>
 nnoremap <leader><C-t>g <cmd>Telescope git_bcommits<Cr>
 nnoremap <leader><C-t>r <cmd>Telescope lsp_references<Cr>
