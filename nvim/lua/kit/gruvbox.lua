026fee7bbf6982c11e4c725d510b0e6f2315bee0
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2021-11-01 09:31:34 +0000

    Configure yaml lsp server

--- nvim/lua/kit/gruvbox.lua
+++ nvim/lua/kit/gruvbox.lua
@@ -13,7 +13,7 @@ vim.cmd([[
 if has('termguicolors')
     set termguicolors
 endif
-let g:gruvbox_material_palette = 'original'
+let g:gruvbox_material_palette = 'mix'
 set background=dark
 let g:gruvbox_material_background = 'hard'
 let g:gruvbox_material_enable_italic = 1
--- nvim/lua/kit/lsp.lua
+++ nvim/lua/kit/lsp.lua
@@ -2,24 +2,12 @@ require'lspconfig'.pyright.setup{}
 
 require'lspconfig'.gopls.setup{}
 
-require'lspconfig'.yamlls.setup(
-  
-    cmd = { "yaml-language-server", "--stdio" }
-    filetypes = { "yaml" }
-    root_dir = root_pattern(".git") or dirname
-    settings = {
-        yaml = {
-            format = {
-                enable = false
-            }
-        }
-      redhat = {
-        telemetry = {
-          enabled = false
-        }
-      }
-    }
-)
-
 require'lspconfig'.clojure_lsp.setup{}
 
+require('lspconfig').yamlls.setup {
+    settings = {
+        format = {
+            enabled = false
+        },
+    },
+}
