35cafb3cc484e2bbf8d5651a35fbab7145066aa5
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2021-11-09 12:51:52 +0000

    More QoL keymaps

--- nvim/lua/kit/keymaps.lua
+++ nvim/lua/kit/keymaps.lua
@@ -93,4 +93,6 @@ vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
   {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap("n", "<leader>|", "<C-^>",{noremap = true})
+vim.api.nvim_set_keymap("n", "|", "^",{noremap = true})
+vim.api.nvim_set_keymap("n", "@", "$",{noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>^", "<C-^>",{noremap = true})
