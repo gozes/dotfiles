5bfc1dff8f277f33e2bda68c66a89f01c6b4d41d
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2021-11-17 09:10:36 +0000

    Add QoL keymaps in visual mode as well

--- nvim/lua/kit/keymaps.lua
+++ nvim/lua/kit/keymaps.lua
@@ -97,3 +97,9 @@ vim.api.nvim_set_keymap("n", "<leader>|", "<C-^>",{noremap = true})
 vim.api.nvim_set_keymap("n", "|", "^",{noremap = true})
 vim.api.nvim_set_keymap("n", "@", "$",{noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>^", "<C-^>",{noremap = true})
+
+vim.api.nvim_set_keymap("v", "<leader>|", "<C-^>",{noremap = true})
+vim.api.nvim_set_keymap("v", "|", "^",{noremap = true})
+vim.api.nvim_set_keymap("v", "@", "$",{noremap = true})
+vim.api.nvim_set_keymap("v", "<leader>^", "<C-^>",{noremap = true})
+
