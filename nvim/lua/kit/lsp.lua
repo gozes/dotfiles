bfbc9b8ee3a7a5c1478ffd0c1d9d35995f7bbcaa
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2021-12-23 15:44:38 +0000

    Nvim: make sure null-ls has been imported

--- nvim/lua/kit/lsp.lua
+++ nvim/lua/kit/lsp.lua
@@ -29,6 +29,11 @@ lsp_installer.on_server_ready(function(server)
     server:setup(opts)
 end)
 
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
