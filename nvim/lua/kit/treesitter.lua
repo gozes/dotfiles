04d89dbb428d90c7eeb10e4421ebd9bbb1711471
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2022-04-25 09:49:01 +0100

    Nvim: Update treesitter config

--- nvim/lua/kit/treesitter.lua
+++ nvim/lua/kit/treesitter.lua
@@ -2,7 +2,7 @@ local parsers = require("nvim-treesitter.parsers")
 
 local enabled_list = {"clojure", "fennel","racket"}
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
   enable = true,              -- false will disable the whole extension
     disable = { "c", "rust" },  -- list of language that will be disabled
