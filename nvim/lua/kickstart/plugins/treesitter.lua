79554ec1895d688e0cbae76a3ef2099a45e3fe02
gozes <ja.sanchezmontalvo@gmail.com>
2024-05-06 10:12:57 +0100

    Nvim: add yaml, json, and gomod treesitter parsers

--- nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/lua/kickstart/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'fish' },
+    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'fish', 'yaml', 'json', 'gomod' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
