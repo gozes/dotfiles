29f7583f4fc921d77e4593b0acd1c198bc59b383
gozes <ja.sanchezmontalvo@gmail.com>
2024-05-13 09:44:15 +0100

    Fish: add homebrew setup to set path

--- fish/config.fish
+++ fish/config.fish
@@ -113,3 +113,22 @@ if test -d $HOME/.asdf
   source $HOME/.asdf/asdf.fish
   source $HOME/.asdf/completions/asdf.fish
 end
+# https://fishshell.com/docs/current/cmds/status.html
+# https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316
+# https://github.com/ublue-os/bluefin/issues/687#issuecomment-1913341899
+if status is-interactive
+    if test -d /home/linuxbrew/.linuxbrew # Linux
+	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+    else if test -d /opt/homebrew # MacOS
+	set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+    end
+    if test -n "$HOMEBREW_PREFIX"
+        fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+        ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+        ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+    end
+end
