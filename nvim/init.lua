02f1d5c0803b95052c84d164846d72b6408a66c8
gozes <ja.sanchezmontalvo@gmail.com>
2024-05-22 15:34:01 +0100

    Nvim: add suppress_missing_scope to Octo config

--- nvim/init.lua
+++ nvim/init.lua
@@ -383,7 +383,7 @@ require('lazy').setup({
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require('octo').setup { enable_builtin = true }
+      require('octo').setup { enable_builtin = true, suppress_missing_scope = { projects_v2 = true } }
       vim.cmd [[hi OctoEditable guibg=none]]
       vim.keymap.set('n', '<leader>O', '<cmd>Octo<cr>', { desc = 'Octo' })
     end,
