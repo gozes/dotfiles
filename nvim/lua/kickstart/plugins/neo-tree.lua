8158f880d8cada14a96284064ca37051db468c3f
gozes <ja.sanchezmontalvo@gmail.com>
2024-05-27 15:35:27 +0100

    Nvim: move to nvim tree to try it out

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,43 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  'nvim-tree/nvim-tree.lua',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local nvimtree = require 'nvim-tree'
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup {
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
         },
       },
-    },
-  },
+      filters = {
+        custom = { '.DS_Store' },
+      },
+      git = {
+        ignore = false,
+      },
+    }
+
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set('n', '<leader>xx', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' }) -- toggle file explorer
+    keymap.set('n', '<leader>xe', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle file explorer on current file' }) -- toggle file explorer on current file
+    keymap.set('n', '<leader>xc', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' })
+    keymap.set('n', '<leader>xr', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' }) -- refresh file explorer
+  end,
 }
