c5f0c6b8c4941e917dcee948c2a031aaca33c87d
gozes <ja.sanchezmontalvo@gmail.com>
2024-06-05 21:49:27 +0100

    Nvim: add dap virtual text and update lazy.lock

--- nvim/.config/nvim/lua/custom/plugins/go.lua
+++ nvim/.config/nvim/lua/custom/plugins/go.lua
@@ -4,6 +4,7 @@ return {
     'ray-x/guihua.lua',
     'neovim/nvim-lspconfig',
     'nvim-treesitter/nvim-treesitter',
+    'theHamsta/nvim-dap-virtual-text',
   },
   config = function()
     require('go').setup {
