0fac86568221adfb82685843dc9f0232f118b335
gozes <632922+gozes@users.noreply.github.com>
2024-07-17 17:12:25 +0100

    Nvim: add pyright to lspconfig

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -187,7 +187,7 @@ return { -- LSP Configuration & Plugins
           },
         },
       },
-      -- pyright = {},
+      pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
