c3dbfd6110fdd97c4e47fefa5e3d39386de8ab39
gozes <632922+gozes@users.noreply.github.com>
2024-10-09 14:48:54 +0100

    Nvim: update copilot pluging to support cmp

--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -1,15 +1,31 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup {}
-  end,
-  cond = function()
-    vim.fn.system 'which gh'
-    if vim.v.shell_error ~= 0 then
-      return false
-    end
-    return true
-  end,
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {}
+    end,
+    cond = function()
+      vim.fn.system 'which gh'
+      if vim.v.shell_error ~= 0 then
+        return false
+      end
+      return true
+    end,
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    cond = function()
+      vim.fn.system 'which gh'
+      if vim.v.shell_error ~= 0 then
+        return false
+      end
+      return true
+    end,
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+    },
+    config = true,
+  },
 }
