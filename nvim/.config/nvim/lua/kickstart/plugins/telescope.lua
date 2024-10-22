30b56807ea71c5727817681a2e670501334803e0
gozes <632922+gozes@users.noreply.github.com>
2024-10-22 11:13:02 +0100

    Nvim: add man page 2 and 3 for telescope man page search

--- nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
@@ -68,7 +68,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sm', builtin.man_pages, { desc = '[S]earch [M]an pages' })
+    vim.keymap.set('n', '<leader>sm', function()
+      builtin.man_pages { sections = { '1', '2', '3' } }
+    end, { desc = '[S]earch [M]an pages' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sS', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
