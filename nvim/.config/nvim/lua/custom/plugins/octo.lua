03a557ba7b32c1b41278c4ff6b9cf3d9abb5958f
gozes <632922+gozes@users.noreply.github.com>
2024-10-25 15:03:25 +0100

    Nvim: fix overlapping octo keymaps

--- nvim/.config/nvim/lua/custom/plugins/octo.lua
+++ nvim/.config/nvim/lua/custom/plugins/octo.lua
@@ -8,9 +8,9 @@ return {
   config = function()
     require('octo').setup { enable_builtin = true, suppress_missing_scope = { projects_v2 = true } }
     vim.cmd [[hi OctoEditable guibg=none]]
-    vim.keymap.set('n', '<leader>Gpl', '<cmd>Octo pr list<cr>', { desc = 'Octo: list PRs' })
-    vim.keymap.set('n', '<leader>Gpc', '<cmd>Octo pr changes<cr>', { desc = 'Octo: Show all PR changes (diff hunks)' })
-    vim.keymap.set('n', '<leader>Gpc', '<cmd>Octo pr checks<cr>', { desc = 'Octo: Show the status of all checks run on the PR' })
+    vim.keymap.set('n', '<leader>Gl', '<cmd>Octo pr list<cr>', { desc = 'Octo: list PRs' })
+    vim.keymap.set('n', '<leader>Gc', '<cmd>Octo pr changes<cr>', { desc = 'Octo: Show all PR changes' })
+    vim.keymap.set('n', '<leader>Ga', '<cmd>Octo pr checks<cr>', { desc = 'Octo: Show GHAs' })
     vim.keymap.set('n', '<leader>Gpr', '<cmd>Octo pr reload<cr>', { desc = 'Octo: Reload PR' })
     vim.keymap.set('n', '<leader>GR', '<cmd>Octo reaction<cr>', { desc = 'Octo: Add reaction' })
     vim.keymap.set('n', '<leader>Grs', '<cmd>Octo review start<cr>', { desc = 'Octo: Start review' })
@@ -18,7 +18,7 @@ return {
     vim.keymap.set('n', '<leader>Grr', '<cmd>Octo review resume<cr>', { desc = 'Octo: Resume review' })
     vim.keymap.set('n', '<leader>Grc', '<cmd>Octo review comments<cr>', { desc = 'Octo: Show pending comments' })
     vim.keymap.set('n', '<leader>GrC', '<cmd>Octo review close<cr>', { desc = 'Octo: Close the review window and return to the PR' })
-    vim.keymap.set('n', '<leader>Gc', '<cmd>Octo comment add<cr>', { desc = 'Octo: Add a new comment' })
+    vim.keymap.set('n', '<leader>GC', '<cmd>Octo comment add<cr>', { desc = 'Octo: Add a new comment' })
   end,
   cond = function()
     vim.fn.system 'which gh'
