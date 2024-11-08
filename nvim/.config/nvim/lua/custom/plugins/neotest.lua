06209f7f5ccdca23697aad0970681741bcc0afce
gozes <632922+gozes@users.noreply.github.com>
2024-11-08 15:29:07 +0000

    Nvim: add neotest

---
+++ nvim/.config/nvim/lua/custom/plugins/neotest.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      { 'fredrikaverpil/neotest-golang', version = '*' }, -- Installation
+      { 'nvim-neotest/neotest-python' },
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-golang', -- Registration
+          require 'neotest-python',
+        },
+      }
+    end,
+    keys = {
+      {
+        '<leader>tn',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = 'Neotest: run nearest test',
+      },
+      {
+        '<leader>tf',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = 'Neotest: run file test',
+      },
+      {
+        '<leader>td',
+        function()
+          require('neotest').run.run { vim.fn.expand '%', strategy = 'dap' }
+        end,
+        desc = 'Neotest: run file test with debug',
+      },
+      {
+        '<leader>tw',
+        function()
+          require('neotest').watch.toggle(vim.fn.expand '%')
+        end,
+        desc = 'Neotest: start/stop watching test for file',
+      },
+    },
+  },
+}
