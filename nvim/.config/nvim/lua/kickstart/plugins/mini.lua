c9064108130bae0a2c09de951a90725fbef84807
gozes <632922+gozes@users.noreply.github.com>
2024-11-08 15:29:39 +0000

    Nvim: disable mini animation becaues they are laggy and buggy

--- nvim/.config/nvim/lua/kickstart/plugins/mini.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/mini.lua
@@ -1,7 +1,6 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
-    require('mini.animate').setup()
     require('mini.files').setup()
     -- Better Around/Inside textobjects
     --
