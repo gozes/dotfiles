7140c2add394efcaf42ba2669e9833802b05acbb
gozes <632922+gozes@users.noreply.github.com>
2024-11-08 17:46:49 +0000

    Nvim: fix code outline keymap

--- nvim/.config/nvim/lua/custom/plugins/aerial.lua
+++ nvim/.config/nvim/lua/custom/plugins/aerial.lua
@@ -16,6 +16,6 @@ return {
       end,
     }
     -- You probably also want to set a keymap to toggle aerial
-    vim.keymap.set('n', ';cc', '<cmd>AerialToggle!<CR>', { desc = 'Show code outline' })
+    vim.keymap.set('n', ';cl', '<cmd>AerialToggle!<CR>', { desc = 'Show code outline' })
   end,
 }
