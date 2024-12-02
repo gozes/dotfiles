76645cd0d7fd76063f92fc7ee7e4c1bbfd6de3c5
gozes <632922+gozes@users.noreply.github.com>
2024-12-02 09:28:47 +0000

    Nvim: add timers for writing

--- nvim/.config/nvim/lua/custom/plugins/pomodoro.lua
+++ nvim/.config/nvim/lua/custom/plugins/pomodoro.lua
@@ -10,9 +10,19 @@ return {
   keys = {
     { '<leader>p1', '<cmd>TimerSession p1<CR>', desc = 'Pomo: One Round' },
     { '<leader>p2', '<cmd>TimerSession p2<CR>', desc = 'Pomo: Two Rounds' },
+    { '<leader>pew', '<cmd>TimerSession ew<CR>', desc = 'Pomo: Exploratory Writing' },
+    { '<leader>peg', '<cmd>TimerSession eg<CR>', desc = 'Pomo: Evergreen Note' },
   },
   opts = {
     sessions = {
+      ew = {
+        { name = 'Exploratory Writing', duration = '10m' },
+      },
+      eg = {
+        { name = 'Write Evergreen Note', duration = '15m' },
+        { name = 'Read and Think about next step', duration = '5m' },
+        { name = 'Refactor Note', duration = '5m' },
+      },
       p1 = {
         { name = 'Focus', duration = '25m' },
         { name = 'Short Break', duration = '5m' },
