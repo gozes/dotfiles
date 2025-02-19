9918c338c6790ce50148f043a7ccd49d8b2ea81b
gozes <632922+gozes@users.noreply.github.com>
2025-02-19 17:52:19 +0000

    Nvim: added aws doc prompt for ai

--- nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -1,6 +1,52 @@
+local propms = {
+  ['AWS Docs'] = {
+    strategy = 'chat',
+    description = 'Get AWS docs with CF exmaples',
+    opts = {
+      modes = { 'n' },
+      short_name = 'awsdocs',
+      user_prompt = true,
+    },
+    prompts = {
+      role = 'user',
+      content = function(context)
+        local code = require('codecompanion.helpers.actions').get_code(context.start_line, context.end_line)
+        return string.format(
+          [[
+        <user_prompt>
+        When replying to the question follow this steps.
+        1. Only use the Most up to date official AWS documentation or offical aws github repo
+        2. Ensure that you have look at the code in buffre %d and the code below
+        3. When replying make sure that the answer incueld a solution using aws cloudformation
+
+        ```%s
+          %s
+        ```
+        </user_prompt>
+        ]],
+          context.bufnr,
+          context.filetype,
+          code
+        )
+      end,
+    },
+  },
+}
 return {
   'olimorris/codecompanion.nvim',
   keys = {
+    {
+      'n',
+      '<M-a>i',
+      ':CodeCompanion #buffer ',
+      desc = 'CodeCompanion: Inline chat',
+    },
+    {
+      'v',
+      '<M-a>i',
+      ":'<,'>CodeCompanion",
+      desc = 'CodeCompanion: Visual Inline chat',
+    },
     {
       '<M-a>c',
       '<cmd>CodeCompanionChat Toggle<cr>',
@@ -13,6 +59,7 @@ return {
     },
   },
   opts = {
+    prompt_library = propms,
     adapters = {
       copilot = function()
         return require('codecompanion.adapters').extend('copilot', {
