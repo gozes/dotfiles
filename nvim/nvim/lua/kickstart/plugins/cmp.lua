af222b81adecec682a691e0e3ae05c67eee54733
gozes <632922+gozes@users.noreply.github.com>
2025-04-16 10:22:12 +0100

    Nvim: change back and forth snippit keymap

--- nvim/nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/nvim/lua/kickstart/plugins/cmp.lua
@@ -86,12 +86,12 @@ return { -- Autocompletion
         --
         -- <c-l> will move you to the right of each of the expansion locations.
         -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
+        ['<C-x>l'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           end
         end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
+        ['<C-x>h'] = cmp.mapping(function()
           if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           end
