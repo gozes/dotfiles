cc3a65d1d1c02b86c1bad3dbe4a8c96186a91369
gozes <632922+gozes@users.noreply.github.com>
2025-04-23 10:03:25 +0100

    Nvim: make date keymaps buffer local

--- nvim/nvim/after/ftplugin/markdown.lua
+++ nvim/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 --vim.keymap.set('i', '[[', '<cmd>Telekasten insert_link<CR>')
 vim.opt_local.textwidth = 80
-vim.keymap.set('n', '<leader>DD', ':InsertDate<CR>', { noremap = true, silent = true, desc = 'Insert Date' })
-vim.keymap.set('n', '<leader>DT', ':InsertTime<CR>', { noremap = true, silent = true, desc = 'insert Time' })
-vim.keymap.set('n', '<leader>DDT', ':InsertDateTime<CR>', { noremap = true, silent = true, desc = 'Insert DateTime' })
+vim.keymap.set('n', '<leader>DD', ':InsertDate<CR>', { noremap = true, silent = true, buffer = true, desc = 'Insert Date' })
+vim.keymap.set('n', '<leader>DT', ':InsertTime<CR>', { noremap = true, silent = true, buffer = true, desc = 'insert Time' })
+vim.keymap.set('n', '<leader>DDT', ':InsertDateTime<CR>', { noremap = true, silent = true, buffer = true, desc = 'Insert DateTime' })
