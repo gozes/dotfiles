e6771416c931d798b48598a0d71871e63513750c
gozes <632922+gozes@users.noreply.github.com>
2025-06-02 09:46:47 +0100

    Nvim: move avante to use gpt-4.5

--- nvim/nvim/lua/custom/plugins/avante.lua
+++ nvim/nvim/lua/custom/plugins/avante.lua
@@ -45,7 +45,7 @@ return {
     },
     provider = 'copilot',
     copilot = {
-      model = 'claude-3.7-sonnet',
+      model = 'gpt-4.5',
     },
     behaviour = {
       enable_token_counting = false,
