d83c94a1aff8c0df19feffff8defaa3781c00275
gozes <632922+gozes@users.noreply.github.com>
2025-06-16 12:54:36 +0100

    Nvim: redo my color theam to try to go more minimal as test

--- nvim/nvim/lua/kickstart/plugins/catppuccin.lua
+++ nvim/nvim/lua/kickstart/plugins/catppuccin.lua
@@ -6,13 +6,66 @@ return { -- You can easily change to a different colorscheme.
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
+  config = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'catppuccin-mocha'
-
+    --
+    --
+    textcolor = '#cdd6f4'
+    require('catppuccin').setup {
+      highlight_overrides = {
+        all = function(colors)
+          return {
+            ['@parameter'] = { fg = textcolor },
+            ['@text'] = { fg = textcolor },
+            ['@field'] = { fg = textcolor },
+            ['@property'] = { fg = textcolor },
+            ['@module'] = { fg = textcolor },
+            ['@variable.parameter'] = { fg = textcolor },
+            ['@variable.member'] = { fg = textcolor },
+            ['@variable'] = { fg = textcolor },
+            ['@type'] = { fg = textcolor },
+            ['@type.builtin'] = { fg = textcolor },
+            ['@keyword.function'] = { fg = textcolor },
+            ['@keyword.conditional'] = { fg = '#cba6f7' },
+            ['@keyword.repeat'] = { fg = '#cba6f7' },
+            ['@keyword.return'] = { fg = '#c2eebd' },
+            ['@function.call'] = { fg = textcolor },
+            ['@function.builtin'] = { fg = textcolor },
+            ['@boolean'] = { fg = textcolor },
+            ['@number'] = { fg = '#fbc19d' },
+            ['@constant.builtin'] = { fg = textcolor },
+            ['@comment'] = { fg = '#bcc5fe' },
+            ['@spell'] = { fg = '#ebbaef' },
+            ['@string'] = { fg = '#ebbaef' },
+            Function = { fg = textcolor },
+            Keyword = { fg = textcolor },
+            Character = { fg = textcolor },
+            Constant = { fg = textcolor },
+            Special = { fg = textcolor },
+            Operator = { fg = textcolor },
+            Identifier = { fg = textcolor },
+            Type = { fg = textcolor },
+            Structure = { fg = textcolor },
+            Label = { fg = textcolor },
+            Exception = { fg = textcolor },
+            PreProc = { fg = textcolor },
+            Include = { fg = textcolor },
+            Macro = { fg = textcolor },
+            StorageClass = { fg = textcolor },
+            Boolean = { fg = '#fcceb2' },
+            Number = { fg = '#fcceb2' },
+            String = { fg = '#ebbaef' },
+            Comment = { fg = '#b4befe' },
+            Conditional = { fg = '#bac2de' },
+            Repeat = { fg = '#bac2de' },
+          }
+        end,
+      },
+    }
     -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    -- vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
--- nvim/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
-  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', 'nvim-treesitter/playground' },
   build = ':TSUpdate',
   opts = {
     ensure_installed = {
