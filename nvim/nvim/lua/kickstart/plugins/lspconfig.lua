13ffd9b2aaa6f25ff9222b2ab619b7abc15583ef
gozes <632922+gozes@users.noreply.github.com>
2025-06-19 15:08:09 +0100

    enable hints in golang again and comment out golangci-ci-lint lsp. also revert changes to mason config

--- nvim/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -194,7 +194,7 @@ return { -- LSP Configuration & Plugins
       --     },
       --   },
       -- },
-      golangci_lint_ls = {},
+      -- golangci_lint_ls = {},
       -- markdown_oxide = {
       --   capabilities = {
       --     workspace = {
@@ -218,15 +218,15 @@ return { -- LSP Configuration & Plugins
       gopls = {
         settings = {
           gopls = {
-            -- hints = {
-            --   assignVariableTypes = true,
-            --   compositeLiteralFields = true,
-            --   compositeLiteralTypes = true,
-            --   constantValues = true,
-            --   functionTypeParameters = true,
-            --   parameterNames = true,
-            --   rangeVariableTypes = true,
-            -- },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
             analyses = {
               shadow = true,
             },
@@ -364,8 +364,8 @@ return { -- LSP Configuration & Plugins
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
+      -- ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+      -- automatic_installation = false,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
