3285d769a7e88c4945297300c18a76c6b5a5b8fb
gozes <632922+gozes@users.noreply.github.com>
2025-06-25 14:43:49 +0100

    Nvim: put blink menu in the correct place

--- nvim/nvim/lua/kickstart/plugins/blink.lua
+++ nvim/nvim/lua/kickstart/plugins/blink.lua
@@ -63,16 +63,6 @@ return {
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
 
-      menu = {
-
-        -- nvim-cmp style menu
-        draw = {
-          columns = {
-            { 'label', 'label_description', gap = 1 },
-            { 'kind_icon', 'kind' },
-          },
-        },
-      },
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
@@ -80,6 +70,16 @@ return {
       },
 
       completion = {
+        menu = {
+
+          -- nvim-cmp style menu
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
+          },
+        },
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
