eb029f9a602ec1f48e475489cbac37841e7caa13
gozes <632922+gozes@users.noreply.github.com>
2025-07-03 13:08:59 +0100

    Nvim: update go config

--- nvim/nvim/lua/custom/plugins/go.lua
+++ nvim/nvim/lua/custom/plugins/go.lua
@@ -4,17 +4,21 @@ return {
     'ray-x/guihua.lua',
     'neovim/nvim-lspconfig',
     'nvim-treesitter/nvim-treesitter',
-    'theHamsta/nvim-dap-virtual-text',
   },
-  config = function()
-    require('go').setup {
-      gotests_template = 'testify',
-      goimports = 'gopls',
-      gofmt = 'goimports',
-      lsp_inlay_hints = {
-        enable = true,
-      },
-    }
+  opts = {
+    -- lsp_keymaps = false,
+    -- other options
+  },
+  config = function(lp, opts)
+    require('go').setup(opts)
+    local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.go',
+      callback = function()
+        require('go.format').goimports()
+      end,
+      group = format_sync_grp,
+    })
   end,
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
--- nvim/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/nvim/lua/kickstart/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'theHamsta/nvim-dap-virtual-text',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
