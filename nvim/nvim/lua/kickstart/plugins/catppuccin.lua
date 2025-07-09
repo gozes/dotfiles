48413cf1f093d28a3e52be2f54a8a7cdd46e2488
gozes <632922+gozes@users.noreply.github.com>
2025-07-09 12:56:54 +0100

    Nvimt: try out jblow collors

--- nvim/nvim/lua/kickstart/plugins/catppuccin.lua
+++ nvim/nvim/lua/kickstart/plugins/catppuccin.lua
@@ -5,6 +5,7 @@ return { -- You can easily change to a different colorscheme.
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'catppuccin/nvim',
   name = 'catppuccin',
+  enabled = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     -- Load the colorscheme here.
---
+++ nvim/nvim/lua/kickstart/plugins/naysayer.lua
@@ -0,0 +1,9 @@
+return {
+  'RostislavArts/naysayer.nvim',
+  enabled = false,
+  priority = 1000,
+  lazy = false,
+  config = function()
+    vim.cmd.colorscheme 'naysayer'
+  end,
+}
