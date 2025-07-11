b4a80e1ca7740e08a595370a2bf2e2b206d59b28
gozes <632922+gozes@users.noreply.github.com>
2025-07-11 12:52:13 +0100

    Nvim: added yaml nvim to my config

--- nvim/nvim/after/ftplugin/yaml.lua
+++ nvim/nvim/after/ftplugin/yaml.lua
@@ -1,1 +1,5 @@
 vim.opt_local.cursorcolumn = true
+vim.keymap.set('n', ',s', '<cmd>YAMLSnacks<cr>', { desc = 'YAML: Search', buffer = true })
+vim.keymap.set('n', ',yv', '<cmd>YAMLYankValue<cr>', { desc = 'YAML: Yank value full path', buffer = true })
+vim.keymap.set('n', ',yk', '<cmd>YAMLYankKey<cr>', { desc = 'YAML: Yank key full path', buffer = true })
+vim.keymap.set('n', ',v', '<cmd>YAMLView<cr>', { desc = 'YAML: View', buffer = true })
