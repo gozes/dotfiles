bd887ec5b099aab020d2893c6ff9a0fca3ec08b3
gozes <632922+gozes@users.noreply.github.com>
2025-07-12 21:34:50 +0100

    Nvim: test out only showing the complition menu by key map but show ghost test of the thing I'm looking for

--- nvim/nvim/lua/kickstart/plugins/blink.lua
+++ nvim/nvim/lua/kickstart/plugins/blink.lua
@@ -70,7 +70,9 @@ return {
       },
 
       completion = {
+        ghost_text = { enabled = true, show_with_menu = false },
         menu = {
+          auto_show = false,
 
           -- nvim-cmp style menu
           draw = {
