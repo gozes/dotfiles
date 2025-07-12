cc1904712dff75c152b031aceacf151e8074520a
gozes <632922+gozes@users.noreply.github.com>
2025-07-12 22:15:24 +0100

    Nvim: auto show completion menu

--- nvim/nvim/lua/kickstart/plugins/blink.lua
+++ nvim/nvim/lua/kickstart/plugins/blink.lua
@@ -72,7 +72,6 @@ return {
       completion = {
         ghost_text = { enabled = true, show_with_menu = false },
         menu = {
-          auto_show = false,
 
           -- nvim-cmp style menu
           draw = {
