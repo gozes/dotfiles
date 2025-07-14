9173d1f5485929205b1cc7274d9f92cbe0bb4d84
gozes <632922+gozes@users.noreply.github.com>
2025-07-14 09:57:45 +0100

    Nvim: make mason load very lazy to make boot time faster

--- nvim/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -15,12 +15,13 @@ return {
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
-      'mason-org/mason-lspconfig.nvim',
+      { 'mason-org/mason-lspconfig.nvim', event = 'VeryLazy' },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
