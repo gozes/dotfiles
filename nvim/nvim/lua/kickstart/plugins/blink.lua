59980d93d197a37e9f263fa5a456bfbb734ad521
gozes <632922+gozes@users.noreply.github.com>
2025-07-14 09:58:12 +0100

    Nvim: back to not showing the menu after listening to DHH on Lex fridman

--- nvim/nvim/lua/kickstart/plugins/blink.lua
+++ nvim/nvim/lua/kickstart/plugins/blink.lua
@@ -72,6 +72,7 @@ return {
       completion = {
         ghost_text = { enabled = true, show_with_menu = false },
         menu = {
+          auto_show = false,
 
           -- nvim-cmp style menu
           draw = {
