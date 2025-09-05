53691c919b7188c715ea335fe8feb1a4d8084c92
gozes <632922+gozes@users.noreply.github.com>
2025-09-05 22:28:44 +0100

    Nvim: fix venv select warning

--- nvim/nvim/lua/custom/plugins/venvselector.lua
+++ nvim/nvim/lua/custom/plugins/venvselector.lua
@@ -1,6 +1,7 @@
 return {
   'linux-cultist/venv-selector.nvim',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  branch = 'main',
   opts = {
     name = { 'venv', '.venv' },
     dap_enabled = true,
