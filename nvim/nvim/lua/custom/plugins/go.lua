e9036fd89f12ac68c81db4381eb9a41c51bfbbb3
gozes <632922+gozes@users.noreply.github.com>
2025-10-15 12:05:56 +0100

    Nvim: set filetype for a bunch of go files

--- nvim/nvim/lua/custom/plugins/go.lua
+++ nvim/nvim/lua/custom/plugins/go.lua
@@ -24,6 +24,6 @@ return {
     })
   end,
   event = { 'CmdlineEnter' },
-  ft = { 'go', 'gomod' },
+  ft = { 'go', 'gomod', 'gosum', 'gotmpl', 'gohtmltmpl', 'gotexttmpl' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
