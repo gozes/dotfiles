bd1df7a8270729fa65834fc8e405d4cd078e6443
Juan A. Sanchez <632922+gozes@users.noreply.github.com>
2026-06-29 10:37:27 +0100

    Nvim: update config

--- nvim/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -29,6 +29,7 @@ return {
 
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
+      'L3MON4D3/LuaSnip',
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -60,6 +61,8 @@ return {
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
+      require('luasnip').setup {}
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -213,8 +216,8 @@ return {
         -- jdtls = {},
         zk = {},
         -- pyright = {},
-        -- ty = {},
-        pyrefly = {},
+        ty = {},
+        -- pyrefly = {},
         vale_ls = {
           filetypes = { 'asciidoc', 'markdown', 'text', 'tex', 'rst', 'html', 'xml', 'python', 'go', 'gitcommit' },
           root_markers = { '.vale.ini', '.git', '.jj' },
--- nvim/nvim/lua/kickstart/plugins/mini.lua
+++ nvim/nvim/lua/kickstart/plugins/mini.lua
@@ -1,5 +1,5 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
+  'nvim-mini/mini.nvim',
   event = 'VeryLazy',
   config = function()
     local diff = require 'mini.diff'
